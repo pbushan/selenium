2c90cb5c9f2f87da050654c391a6331cfab003f3
pbushan <54369428+pbushan@users.noreply.github.com>
2019-08-23 11:55:10 -0400

    Update ubuntu-docker-seleniumgrid.sh

--- ubuntu-docker-seleniumgrid.sh
+++ ubuntu-docker-seleniumgrid.sh
@@ -8,10 +8,10 @@ sudo apt remove --yes docker docker-engine docker.io \
     && curl -SsL https://get.docker.com | sh \
     && printf '\nDocker installed successfully\n\n'
 
-&& printf 'Waiting for Docker to start...\n\n'
-&& sleep 10
+printf 'Waiting for Docker to start...\n\n'
+sleep 10
 # Get out of using sudo each time
-&& sudo usermod -aG docker ubuntu \
+sudo usermod -aG docker ubuntu \
 
 # Docker Compose
 && sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
