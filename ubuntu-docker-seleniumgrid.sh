c177c06aeffc7cd4132d719ffe4bacb070f8b805
pbushan <54369428+pbushan@users.noreply.github.com>
2019-08-23 11:53:04 -0400

    Update ubuntu-docker-seleniumgrid.sh

--- ubuntu-docker-seleniumgrid.sh
+++ ubuntu-docker-seleniumgrid.sh
@@ -8,20 +8,20 @@ sudo apt remove --yes docker docker-engine docker.io \
     && curl -SsL https://get.docker.com | sh \
     && printf '\nDocker installed successfully\n\n'
 
-printf 'Waiting for Docker to start...\n\n'
-sleep 3
+&& printf 'Waiting for Docker to start...\n\n'
+&& sleep 10
 # Get out of using sudo each time
-sudo usermod -aG docker ubuntu \
+&& sudo usermod -aG docker ubuntu \
 
 # Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose\
-    sudo chmod +x /usr/local/bin/docker-compose \
+&& sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+    && sudo chmod +x /usr/local/bin/docker-compose \
     && printf '\nDocker Compose installed successfully\n\n'
 # Download prometheus.yml
-sudo curl https://raw.githubusercontent.com/pbushan/selenium/master/prometheus.yml > prometheus.yml\
+&& sudo curl https://raw.githubusercontent.com/pbushan/selenium/master/prometheus.yml > prometheus.yml\
 # Install Selenium Grid
-curl https://raw.githubusercontent.com/pbushan/selenium/master/servergrid.yaml > servergrid.yaml \
-sudo docker-compose -f servergrid.yaml up -d \
+&& curl https://raw.githubusercontent.com/pbushan/selenium/master/servergrid.yaml > servergrid.yaml \
+&& sudo docker-compose -f servergrid.yaml up -d \
 
 #Scale the chrome nodes per instance
-sudo docker-compose -f servergrid.yaml scale chrome=10
+&& sudo docker-compose -f servergrid.yaml scale chrome=10
