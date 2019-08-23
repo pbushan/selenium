d9398dfb587ddcd45eb7d2598ee86ef2e1f32843
pbushan <54369428+pbushan@users.noreply.github.com>
2019-08-23 16:26:47 -0400

    Update graphana-influx.sh

--- graphana-influx.sh
+++ graphana-influx.sh
@@ -14,7 +14,7 @@ sleep 10
 sudo usermod -aG docker ubuntu \
 
 # Docker Compose
-&& sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose \
     && sudo chmod +x /usr/local/bin/docker-compose \
     && printf '\nDocker Compose installed successfully\n\n'
 
